69bd5cae98e635d9128b1cf32c057124e3aef6be
KPI0 <2337450445@qq.com>
2024-03-09 13:03:34 +0800

    update

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -12,7 +12,7 @@
 # alist
 git clone https://github.com/sbwml/luci-app-alist package/alist
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 
 mkdir package/community
 pushd package/community
@@ -100,4 +100,4 @@ rm -rf nas-packages-luci/luci/luci-app-istorex
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
